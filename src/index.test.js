4dea1c88781da42d631301a87bf06fd8bb0c5a3b
Logan Ellis <mrloganellis@gmail.com>
2019-12-18 12:58:48 -0500

    finished smoke test

--- src/index.test.js
+++ src/index.test.js
@@ -7,11 +7,15 @@ import List from "./List";
 
 it('card renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Card />, div);
+    ReactDOM.render(<Card title={'Test Title'} content={'Test Content'}/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
+
+
+
 it('list renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(<List cards={[]} />, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
