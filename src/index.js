ffd53afc55dc69ae49daf011dd2bdd7fdc36b997
Logan Ellis <mrloganellis@gmail.com>
2019-12-23 13:37:51 -0500

    added functionality to trelloyes app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import STORE from './store'
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+
 
 ReactDOM.render(
-    <App store={STORE} />,
+    <App />,
     document.getElementById('root'),
 )
 
